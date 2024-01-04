23330b5f1300a153d11860c037a6d84581942f99
ETEMESI <samweletemesi2@Gmail.com>
2024-01-05 01:08:44 +0300

    Completed Assignment.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let main = document.main
+
+//methods
+let h1 = document.createElement("h1");
+h1.innerHTML = "ETEMESI is the champion"
+
+main.appendChild(h1);
+// body.removeChild(h1)
+
+
+// console.log(main);
