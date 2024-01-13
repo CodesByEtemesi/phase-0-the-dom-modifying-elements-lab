8e4212909dc0ec2fab24cde751340f355cd26818
ETEMESI <samweletemesi2@Gmail.com>
2024-01-13 03:41:09 +0300

    Did some minor changes.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Write your code here!
-let main = document.main
+let newHeader;
 
-//methods
-let h1 = document.createElement("h1");
-h1.innerHTML = "ETEMESI is the champion"
+document.addEventListener('DOMContentLoaded', function() {
+  // Remove the 'main' DOM node
+  const mainElement = document.querySelector('main');
+  mainElement.remove();
 
-main.appendChild(h1);
-// body.removeChild(h1)
+  // Create a new 'h1' element with id 'victory'
+  newHeader = document.createElement('h1');
+  newHeader.id = 'victory';
 
+  // Set the innerHTML of the new 'h1' element
+  newHeader.innerHTML = "YOUR-NAME is the champion";
 
-// console.log(main);
+  // Append the new 'h1' element to the body
+  document.body.appendChild(newHeader);
+});
+
+  
